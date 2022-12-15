95227e714afb83744644d7dcee179e6ae620c7a7
Othman-99 <a.othman@cloudypedia.com>
2022-12-15 22:06:04 +0200

    Add offer model to the admin page

--- shipment/admin.py
+++ shipment/admin.py
@@ -12,11 +12,15 @@ class TrailerAdmin(admin.ModelAdmin):
 
 class LoadAdmin(admin.ModelAdmin):
     
-    list_display = ["id", "created_by", "shipper", "consignee", "pick_up_date", "delivery_date", "status"]
+    list_display = ["id", "name", "created_by", "shipper", "consignee", "pick_up_date", "delivery_date", "status"]
 
 class ContactAdmin(admin.ModelAdmin):
 
-    list_display = ["id", "origin", "contact"]   
+    list_display = ["id", "origin", "contact"]
+
+class OfferAdmin(admin.ModelAdmin):
+
+    list_display = ["id", "bidder", "receiver", "value"]
 
 
 
@@ -24,3 +28,4 @@ admin.site.register(Facility, admin_class=FacilityAdmin)
 admin.site.register(Trailer, admin_class=TrailerAdmin)
 admin.site.register(Load, admin_class=LoadAdmin)
 admin.site.register(Contact, admin_class=ContactAdmin)
+admin.site.register(Offer, admin_class=OfferAdmin)
