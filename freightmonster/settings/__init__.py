2b64f4613b18a351665cfbbb360504c1fae858aa
AhmedEbrahim99 <sosta9918@yahoo.com>
2023-01-06 21:26:35 +0200

    adjusted the settings file

--- freightmonster/settings/__init__.py
+++ freightmonster/settings/__init__.py
@@ -1,22 +1,21 @@
-# import os
+import os
 
-# from django.core.exceptions import ImproperlyConfigured
+from django.core.exceptions import ImproperlyConfigured
 
-# ENVS = ["LOCAL" ,"DEV", "PROD", "STAGING"]
+ENVS = ["LOCAL" ,"DEV", "PROD", "STAGING"]
 
-# env = os.getenv("ENV")
+env = os.getenv("ENV")
 
-# if env not in ENVS:
-#     error_message = f"The currnet 'ENV' is {env} but must be one of {ENVS}"
-#     raise ImproperlyConfigured(error_message)
+if env not in ENVS:
+    error_message = f"The currnet 'ENV' is {env} but must be one of {ENVS}"
+    raise ImproperlyConfigured(error_message)
 
-# match env:
-#     case "DEV":
-#         from .dev import *
-#     case "PROD":
-#         from .prod import *
-#     case "STAGING":
-#         from .staging import *
-#     case "LOCAL":
-#         from .local import *
-from .local import *
+match env:
+    case "DEV":
+        from .dev import *
+    case "PROD":
+        from .prod import *
+    case "STAGING":
+        from .staging import *
+    case "LOCAL":
+        from .local import *
