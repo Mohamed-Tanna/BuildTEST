4c2ed595a35c566bcd8a6efb934b4c7c59240e08
Othman-99 <a.othman@cloudypedia.com>
2023-01-09 23:58:53 +0200

    remove print

--- shipment/views.py
+++ shipment/views.py
@@ -1035,7 +1035,6 @@ class ShipmentAdminView(
     queryset = ShipmentAdmin.objects.all()
 
     def get(self, request, *args, **kwargs):
-        print("GET")
         return self.list(request, *args, **kwargs)
 
     def post(self, request, *args, **kwargs):
@@ -1080,14 +1079,11 @@ class ShipmentAdminView(
 
     def get_queryset(self):
 
-        print("before assert")
         assert self.queryset is not None, (
             "'%s' should either include a `queryset` attribute, "
             "or override the `get_queryset()` method." % self.__class__.__name__
         )
 
-        print("55555555")
-        print(self.request.GET.get("id"))
         if self.request.GET.get("id"):
             shipment_id = self.request.GET.get("id")
             queryset = ShipmentAdmin.objects.filter(shipment=shipment_id)
