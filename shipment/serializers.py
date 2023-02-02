b4f0708b8e32cbcf2739f02e08899700b7f59e11
AhmedEbrahim99 <sosta9918@yahoo.com>
2023-02-02 16:13:44 +0200

    fixing the load list serializer

--- shipment/serializers.py
+++ shipment/serializers.py
@@ -51,7 +51,11 @@ class LoadListSerializer(serializers.ModelSerializer):
         rep["customer"] = instance.customer.app_user.user.username
         rep["shipper"] = instance.shipper.app_user.user.username
         rep["consignee"] = instance.consignee.app_user.user.username
-        rep["carrier"] = instance.carrier.app_user.user.username
+        try:
+            rep["carrier"] = instance.carrier.app_user.user.username
+        except BaseException as e:
+            print(f"Unexpected {e=}, {type(e)=}")
+            rep["carrier"] = None
         try:
             rep["broker"] = instance.broker.app_user.user.username
         except BaseException as e:
