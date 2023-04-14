a7b4bb7773a5ef6ad6f48494d2c73a348030a565
Othman-99 <a.othman@cloudypedia.com>
2023-04-14 18:04:46 +0200

    ci/cd

--- vm_startup_script.sh
+++ vm_startup_script.sh
@@ -58,6 +58,7 @@ gcloud auth configure-docker \
     us-west1-docker.pkg.dev
 echo y
 
+# Set environment variables
 IMAGE_1=us-west1-docker.pkg.dev/freightmonster-dev/backend/backend:$commit_SHA 
 IMAGE_2=us-west1-docker.pkg.dev/freightmonster-dev/nginx/backend-nginx:$commit_SHA
 export IMAGE_1
