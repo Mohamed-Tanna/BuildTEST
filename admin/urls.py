a184b42753c74dc8d50662f35ea298996f997c9e
Omir11 <omir.hosny1212@premier.cloudypedia.com>
2023-08-23 00:35:51 +0300

    Create a new branch 'retrieve_employees_contacts_view' , and Implemented a contacts-retrival view for the admin port

--- admin/urls.py
+++ admin/urls.py
@@ -1,5 +1,8 @@
-import admin.views as views
 from django.urls import path, include
+import admin.views as views
+
 urlpatterns = [
+
+    path("contacts/", views.ContactsView.as_view(), name="contacts-list"),
     
 ]
--- employees_contacts/views.py
+++ employees_contacts/views.py
@@ -1,13 +1,14 @@
+from django.shortcuts import render
+
+
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import LimitOffsetPagination
 from authentication.models import AppUser, CompanyEmployee
 from authentication.serializers import CompanyEmployeeSerializer
 
-class ContactsView(ListAPIView):
-    permission_classes = [
-        IsAuthenticated,
-        ]
+class RetrieveEmployeesContactsView(ListAPIView):
+    permission_classes = [IsAuthenticated]
     serializer_class = CompanyEmployeeSerializer
     pagination_class = LimitOffsetPagination
 
