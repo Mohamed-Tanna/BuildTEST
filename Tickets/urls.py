8c5422e58e4c35129b885eb41be30668754bd638
Omir11 <omir.hosny1212@premier.cloudypedia.com>
2023-08-30 20:25:58 +0300

    Did the Needed Changes and Migrated everything to the 'Support Module'

--- Tickets/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import TicketListView, TicketCreateView, TicketRetrieveView
-
-urlpatterns = [
-    path('tickets/', TicketListView.as_view(), name='ticket-list'),
-    path('tickets/create/', TicketCreateView.as_view(), name='ticket-create'),
-    path('tickets/<int:id>/', TicketRetrieveView.as_view(), name='ticket-retrieve'),
-]
--- support/urls.py
+++ support/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from support.views import CompanyView
+from .views import ListCreateTicketsView, RetrieveTicketsView
+
 
 
 urlpatterns = [
     path("create-company/", CompanyView.as_view()),
+    path('list-create_tickets/', ListCreateTicketsView.as_view(), name='ticket-creation\listing'),
+    path('retrive_tickets/', RetrieveTicketsView.as_view(), name='ticket-retrieval'),
+    
 ]
