9ef0b630f48961598b3a9f83b6915912ca9efe1b
Mohamed-Tanna <m.wael@cloudypedia.com>
2024-02-09 01:33:38 +0200

    change startup order

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-celery -A freightmonster worker --loglevel=info
-celery -A freightmonster beat --loglevel=info
 python manage.py crontab add
 python manage.py collectstatic
 python manage.py makemigrations --noinput
 python manage.py migrate
 gunicorn freightmonster.wsgi:application --bind 0.0.0.0:8000
+celery -A freightmonster worker --loglevel=info
+celery -A freightmonster beat --loglevel=info
