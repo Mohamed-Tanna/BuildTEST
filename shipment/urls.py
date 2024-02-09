977eb31350cfcb094962fe76079f601d80a97273
Mohamed-Tanna <m.wael@cloudypedia.com>
2024-02-09 13:43:55 +0200

    add cloud schedule endpoint

--- shipment/urls.py
+++ shipment/urls.py
@@ -4,6 +4,7 @@ import shipment.views as views
 from shipment.routers import load_router, shipment_router
 
 urlpatterns = [
+    path("cloud/", views.CloudSchedulerTaskView.as_view()),
     path("load/claim/claim-note/", views.ClaimNoteView.as_view()),
     path("load/claim/", views.ClaimView.as_view()),
     path("load/claim/<id>", views.ClaimView.as_view()),
